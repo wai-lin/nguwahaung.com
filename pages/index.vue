ddfc9c0658340b01be28faaa166d61c1771c97f5
wai-lin <wailin.wilbert@gmail.com>
2024-07-11 07:59:03 +0700

    feat: hero section timeline animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-	<section class="hero1 h-screen max-h-screen bg-yellow-200">
-		<h1>Hero 1</h1>
+	<section gsap="hero" class="h-screen max-h-screen bg-yellow-200">
+		<h1 align="center">Hero 1</h1>
 
-		<div class="hero1-box absolute inset-0 bg-white"></div>
+		<div class="absolute inset-0 flex items-center justify-center gap-10">
+			<div gsap="hero-white_bg" class="absolute h-screen w-full bg-white"></div>
+
+			<div
+				gsap="hero-card_hidden"
+				class="z-10 h-[360px] w-1/5 rounded-lg bg-gray-200 opacity-100"
+			></div>
+			<div
+				gsap="hero-card"
+				class="z-10 h-[500px] w-1/5 rounded-lg border-2 border-gray-400 bg-gray-400 bg-opacity-20"
+			></div>
+			<div
+				gsap="hero-card_hidden"
+				class="z-10 h-[360px] w-1/5 rounded-lg bg-gray-200 opacity-100"
+			></div>
+		</div>
 	</section>
+
 	<section class="hero2 h-screen max-h-screen bg-red-300">Hero 2</section>
 </template>
 
@@ -14,18 +30,33 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 onMounted(() => {
 	gsap.registerPlugin(ScrollTrigger);
 
-	gsap.from(".hero1-box", {
-		scale: 0,
-		transformOrigin: "center bottom",
-		ease: "back.inOut",
-		borderRadius: "50rem",
+	const hero_tl = gsap.timeline({
 		scrollTrigger: {
-			trigger: ".hero1",
+			trigger: `[gsap="hero"]`,
 			scrub: true,
 			pin: true,
 			start: "top top",
 			end: "+=100%",
 		},
 	});
+
+	hero_tl
+		.from(`[gsap="hero-white_bg"]`, {
+			scale: 1,
+			width: "62%",
+			height: "500px",
+			transformOrigin: "center center",
+			ease: "sine",
+			borderRadius: "0.5rem",
+		})
+		.from(`[gsap="hero-card"]`, {
+			width: "75%",
+			ease: "sine",
+		})
+		.from(`[gsap="hero-card_hidden"]`, {
+			y: "20%",
+			opacity: 0,
+			ease: "sine",
+		});
 });
 </script>
